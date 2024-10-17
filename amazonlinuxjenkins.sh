b5e08dff0fda315524584ce9ce23efeb94776ecb
root <root@ip-172-31-4-37.ap-south-1.compute.internal>
2024-10-17 16:15:11 +0000

    amazon linux jenkins installation

---
+++ amazonlinuxjenkins.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+yum update -y
+sudo yum install java-17-amazon-corretto-devel -y
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo dnf upgrade
+# Add required dependencies for the jenkins package
+sudo dnf install fontconfig java-17-openjdk
+sudo dnf install jenkins
+sudo systemctl daemon-reload
